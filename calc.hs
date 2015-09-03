6ade91bd26e3ef7134091497e64f65dceaa87d7c
ftsan <ftsan_12345@gmail.com>
2015-09-04 02:07:49 +0900

    Modified

--- calc.hs
+++ calc.hs
@@ -1,2 +1,11 @@
 mul (i,x) = x * i
 calc xs = foldl (+) 0 (map mul (zip [0..] xs))
+
+c :: Int -> Int
+c i = 2 * i - a * 9 
+    where
+        a = div i 10
+
+main = do
+    a <- readLn
+    print $ c a
--- fibo3.hs
+++ fibo3.hs
@@ -1,12 +1,17 @@
-fibo :: Int -> Int
-fibo 1 = 0
-fibo 2 = 1
-fibo n = fibo (n - 1) + fibo (n - 2)
+memorized_fib :: Int -> Integer
+memorized_fib =
+    let fibo 1 = 0
+        fibo 2 = 1
+        fibo n = memorized_fib (n - 2) + memorized_fib (n - 1)
+        in (map fibo [0 .. ] !!)
+fiboList n = map memorized_fib [1..n]
 
-fiboList :: Int -> [Int]
-fiboList x = map fibo [1..x]
- 
+join :: String -> [String] -> String
+join _ []       = []
+join _ [x]      = x
+join sep (x:xs) = x ++ sep ++ join sep xs
+
 main :: IO ()
 main = do
     arg <- readLn :: IO Int
-    print $ fiboList $ arg
+    putStrLn $ join "," . map show $ fiboList arg
