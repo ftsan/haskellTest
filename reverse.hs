901d24dc312a7f8de3d5be58b58dafe399a05bbf
ftsan <ftsan12345@gmail.com>
2015-12-27 19:05:25 +0900

    Implement reverse method

--- reverse.hs
+++ reverse.hs
@@ -2,5 +2,12 @@ reverse' :: [a] -> [a]
 reverse' []     = []
 reverse' (x:xs) = reverse' xs ++ [x]
 
+reverse'' :: [a] -> [a]
+reverse'' xs = rev xs []
+    where
+        rev [] ys     = ys
+        rev (x:xs) ys = rev xs (x:ys)
+
 main = do
     print $ reverse' [1..5]
+    print $ reverse'' [1..5]
